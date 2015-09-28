f3b2930ecb4817b2d4a1ae61fe0f73fba99da4d0
Matthew Reidsma <reidsmam@gvsu.edu>
2015-09-28 12:01:40 -0400

    Removed explicit NULL timestamp call since it defaults to current on MySQL

--- feedback/send.php
+++ feedback/send.php
@@ -1,7 +1,7 @@
 <?php
 include '../connection.php';
 $emotionId = $_POST['emotionId'];
-$sql = "INSERT INTO feedback_response (response_id, emotion_id, timestamp) VALUES (NULL,$emotionId,'')";
+$sql = "INSERT INTO feedback_response (response_id, emotion_id) VALUES (NULL,$emotionId)";
 if ($con->query($sql)) {
 	echo json_encode(array('success' => true));
 } else {
