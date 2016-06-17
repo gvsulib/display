ca776e0bfc1f88ac2bf9ba58383b0b70cb92dc8d
Matthew Reidsma <reidsmam@gvsu.edu>
2016-06-17 10:25:53 -0400

    Redirected to new computer availability map

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <head>
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
-    <link rel="stylesheet" type="text/css" href="http://gvsuliblabs.com/labs/opac/css/fonts.css" />
+    <link rel="stylesheet" type="text/css" href="//gvsuliblabs.com/labs/opac/css/fonts.css" />
     <link rel="stylesheet" type="text/css" href="css/emojione.min.css" />
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
@@ -31,13 +31,13 @@
 		<!--<h3>GVSU is part of the Google Glass Explorer program. Stop by the Technology Showcase in the Atrium to try out the latest in wearable tech!</h3>-->
         <div id="message"><h4 class="message-post-time"></h4>
         <h4 class="message-heading"></h4><p></p></div>
-        <iframe scrolling="no" width="100%" height="190" id="notifications" src="http://www.gvsu.edu/events/slideshow-index.htm?slideshowId=48DC2DE5-F7C1-B484-FB7A1B7AAD7F9050"></iframe>
+        <iframe scrolling="no" width="100%" height="190" id="notifications" src="https://www.gvsu.edu/events/slideshow-index.htm?slideshowId=48DC2DE5-F7C1-B484-FB7A1B7AAD7F9050"></iframe>
 	</div>
 
 	<div class="computer-availability-container">
 		<h2 data-refresh="2">Library Computer Availability</h2>
 
-		<iframe width="100%" height="250" id="cpumap" style="padding-left: 20px; padding-right: 10px; padding-top: 10px;" src="http://gvsulib.com/labs/computer_availability/?x=true&amp;library=maryi&amp;notitle"></iframe>
+		<iframe width="100%" height="250" id="cpumap" style="padding-left: 20px; padding-right: 10px; padding-top: 10px;" src="http://gvsulibdisplay.com/labs/computer_availability/?x=true&amp;library=maryi&amp;notitle"></iframe>
 	</div>
 
 	<div class="room-availability-container">
