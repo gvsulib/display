05018f0ab747cff4c90d98eceaefb055d14a989e
Matthew Reidsma <reidsmam@gvsu.edu>
2017-04-19 11:28:58 -0400

    Added computer availability map back in

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ $( document ).ready(function() {
     resetButtons();
     getTraffic();
     getRoomAvailability();
+    updateComputerAvailability();
     selectFloor(floor);
     updateTime();
     getMessages();
